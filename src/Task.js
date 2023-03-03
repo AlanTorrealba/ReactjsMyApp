7359c547b71213fa3ddf3ef52b09c9b342157fc6
ant <antorrealbatapia@gamil.com>
2023-03-02 22:40:35 -0400

    2hrs

---
+++ src/Task.js
@@ -0,0 +1,13 @@
+import "./tasks.css";
+
+export function TaskCard({ready}) {
+  return (
+    <div className="card">
+      <h1>MI TASKCARD</h1>
+      <span
+        className={ready ? 'bg-green' : 'bg-red'}
+      > {ready === true ? "Tarea Realizada" : "Tarea Pendiente"}</span>
+      <p>34123412341234</p>
+    </div>
+  );
+}
