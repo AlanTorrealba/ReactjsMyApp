52ea8d7fbb12e23b25b0e6a77510df56c66a3ea9
AlanNoel <antorrealbatapia@gmail.com>
2023-03-03 08:15:21 -0400

    commit

---
+++ src/Task.js
@@ -0,0 +1,8 @@
+export function TaskCard() {
+
+    return <div>
+       <h1>Mi primer tarea</h1>
+       <p>Tarea A Realizar</p>
+    </div>
+    
+}
--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 //import { Greeting } from "./Greeting";
-import { Button } from "./Button";
+//import { Button } from "./Button";
 //import Product from "./Product";
+import { TaskCard } from "./Task";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <>
-   <Button text={"hellow hellow"} />
+    {/* <Button text={"hellow hellow"} />
    <Button text={"hellow hellow"} />
    <Button text='' name = "Jelo"/>
-  
- 
-   {/*  <Greeting
+   */}
+
+    {/*  <Greeting
       tittle="hola"
       propiedad1="12431243"
       propiedad2="12431243"
@@ -22,5 +23,7 @@ root.render(
         alert("EY CUIDADO")
       }}
     /> */}
+
+    <TaskCard />
   </>
 );
